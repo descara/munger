fbe6b07a92222e60ac2849df339d58a28519c14c
Reuben Joseph <Reuben.Joseph@au.anz.ey.net>
2018-05-21 22:01:54 +1000

    Changed .gitignore

--- munger/ParseFeed.py
+++ munger/ParseFeed.py
@@ -13,7 +13,9 @@ ROOT_DIR = os.path.dirname(os.path.abspath('conf/'))
 CONFIG_PATH = os.path.join(ROOT_DIR, 'conf/feeds.conf')
 
 def main():
+    file = open(CONFIG_PATH, 'r')
     feed_config = json.loads(open(file).read())
+    print(feed_config)
 
 if __name__ == "__main__":
     main()
